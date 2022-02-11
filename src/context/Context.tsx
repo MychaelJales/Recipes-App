94825f2bc6e9d85d696d75807406c8643c170855
MychaelJales <mychael_duarte@hotmail.com>
2022-02-11 11:52:51 -0300

    feat: criando context

---
+++ src/context/Context.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, FC, useState } from 'react';
+
+//Tipando os dados que quero para usuário
+type UserType = {
+  email: string;
+};
+
+//Tipando as Props do contexto
+type PropsUserContext = {
+  state: UserType;
+  setState: React.Dispatch<React.SetStateAction<UserType>>;
+};
+
+//Valor default do contexto
+const DEFAULT_VALUE = {
+  state: {
+    email: ''
+  },
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setState: () => {} //função de inicialização
+};
+
+//criando nosso contexto UserContext
+export const CambiosContext = createContext<PropsUserContext>(DEFAULT_VALUE);
+
+export const CambiosProvider: FC = ({ children }) => {
+  const [state, setState] = useState(DEFAULT_VALUE.state);
+
+  return (
+    <CambiosContext.Provider
+      value={{
+        state,
+        setState
+      }}
+    >
+      {children}
+    </CambiosContext.Provider>
+  );
+};
